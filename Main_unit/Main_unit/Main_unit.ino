1a54ae7a7bb8cca326961f4ae4c0f03e73299b2b
Vulkanov <pavel@vulkanov.ru>
2018-01-14 16:32:28 +0300

    Доработка программы
    Блок сенсора:
    оптимизирована функция формирования массива для отправки, добавлена отправка значения яркости.
    Блок увлажнителя:
    1. Оптимизирована функция приема данных, добавлено прием освещенности
    2. Прописана настройка значения влажности с переключением дисплея на настраиваемый параметр
    3. Добавлена индикация включения/выключения подогревателя и ионизатора
    4. Добавлен автоматический режим включения подогрева (при влажности менее 40%)
    5. Оптимизирован код.

--- Main_unit/Main_unit/Main_unit.ino
+++ Main_unit/Main_unit/Main_unit.ino
@@ -11,42 +11,37 @@ SoftwareSerial mySerial(2, 3); // RX, TX
 #define ModePin1 A1
 #define ModePin2 A2
 #define ModePin3 A3
-
-
 TM1628ts disp(9, 8, 7); // define - data pin 8, clock pin 9 and strobe pin 7
 
 // Главные переменные
-
-
-//const byte OFF = 0;
-//const byte ON = 1;
-//const byte NO_WATER = 3; // Питание 
+ 
 bool PWR = LOW;
 byte PWRSTATE = 0; 
 bool WATERSENSOR = LOW;               // Датчик наличия бака и воды LOW = пустой или нет бака
 byte IONSTATE_adress = 40;  
 bool IONSTATE = EEPROM.read(IONSTATE_adress);                         // Ионизатор вкл/выкл
-byte HEATSTATE_adress =45; //Адрес в памяти HEATING
-bool HEATSTATE = EEPROM.read(HEATSTATE_adress);                    // Подогрев вкл/выкл
+byte HEATERSTATE_adress =45; //Адрес в памяти HEATING
+byte HEATERSTATE = EEPROM.read(HEATERSTATE_adress);                    // Подогрев вкл/выкл
 byte HUMPOW_adress = 50;              // Адрес в памяти HUM_MODE
 byte HUMPOW = EEPROM.read(HUMPOW_adress); // Режим работы увлажн. 1-2-3 
 byte HUMVAL_adress = 55;
 byte HUMVAL = EEPROM.read(HUMVAL_adress); //Значение поддержания влажности 40 - 90
 int button; // кнопки управления
 long previousMillis = 0; //счетчик прошедшего времени для AutoMainScreen
-long interval = 30000; //задержка автовозврата к MainScreen 3сек
+long interval = 4000; //задержка автовозврата к MainScreen 3сек
 unsigned long currentMillis;
+byte Display = 0;
 
 byte msg[VW_MAX_MESSAGE_LEN]; // Буфер для хранения принимаемых данных 
 byte msgLen = VW_MAX_MESSAGE_LEN; // Размер сообщения
 int T = 10;
 int H = 10;
 int P = 740;
-int display_intensity = 7;
+byte display_intensity = 7;
 
-const int led_pin = 13; // Пин светодиода 
-const int receiver_pin = 12; // Пин подключения приемника
-const int reset_pin = 10; // при высоком состоянии wi-fi модуль работает, при низком - reset. 3 раза reset - вход в режим настройки.
+const byte led_pin = 13; // Пин светодиода 
+const byte receiver_pin = 12; // Пин подключения приемника
+const byte reset_pin = 10; // при высоком состоянии wi-fi модуль работает, при низком - reset. 3 раза reset - вход в режим настройки.
 IRrecv irrecv(11); // Указываем пин, к которому подключен приемник
 decode_results results;
 
@@ -68,7 +63,7 @@ void setup() {
   pinMode(reset_pin, OUTPUT);
   digitalWrite(reset_pin, HIGH);
 // Приветствие :)
- for (int i = 14; i > 0; i--) {
+ for (int i = 1; i < 10; i++) {
     
     disp.putDigitAt(i, 0);
     disp.putDigitAt(i, 2);
@@ -77,63 +72,57 @@ void setup() {
     disp.putDigitAt(i, 8);
     disp.putDigitAt(i, 10);
     disp.putDigitAt(i, 12);
-    disp.writeBuffer();
-    disp.setStatus(LEVEL_LED1, 1);
-    disp.setStatus(LEVEL_LED2, 1);
-    disp.setStatus(LEVEL_LED3, 1);
-    disp.setStatus(LEVEL_LED4, 1);
-    disp.setStatus(LEVEL_LED5, 1);
-    disp.setStatus(LEVEL_LED6, 1);
-    disp.setStatus(LEVEL_LED7, 1);
-    disp.setStatus(LEVEL_LED8, 1);
-    disp.setStatus(LEVEL_LED9, 1);
-    delay(150);
+    
+    disp.setStatus(LEVEL_LED1, (i > 0 ? 1 : 0));
+    disp.setStatus(LEVEL_LED2, (i > 1 ? 1 : 0));
+    disp.setStatus(LEVEL_LED3, (i > 2 ? 1 : 0));
+    disp.setStatus(LEVEL_LED4, (i > 3 ? 1 : 0));
+    disp.setStatus(LEVEL_LED5, (i > 4 ? 1 : 0));
+    disp.setStatus(LEVEL_LED6, (i > 5 ? 1 : 0));
+    disp.setStatus(LEVEL_LED7, (i > 6 ? 1 : 0));
+    disp.setStatus(LEVEL_LED8, (i > 7 ? 1 : 0));
+    disp.setStatus(LEVEL_LED9, (i > 8 ? 1 : 0));
+    disp.writeBuffer();//disp.setStatus(9, 1);
+    //disp.setStatus(LEVEL_LED3, 1);
+    //disp.setStatus(LEVEL_LED4, 1);
+    //disp.setStatus(LEVEL_LED5, 1);
+    //disp.setStatus(LEVEL_LED6, 1);
+    //disp.setStatus(LEVEL_LED7, 1);
+    //disp.setStatus(LEVEL_LED8, 1);
+    //disp.setStatus(LEVEL_LED9, 1);
+    delay(200);
   }
-
-  delay(300);
-  
+  delay(100);
 }
 
 // ========== ФУНКЦИИ ==========
 
-
-
 // Приемник MX-RM-5V
 // Прием и обработка данных  
- 
-int processing_data(int arrStart, int arrEnd) { // параметр = T или H или P
-  // Принимается массив из 11-ти символов
-  // 0-3 = пароль для защиты от помех
-  // 4-5 = температура
-  // 6-7 = влажность
-  // 8-10 = давление   
-    int number = 0;
-    for (int i = arrStart; i <= arrEnd ; i++) 
-    {  
-      number *= 10;
-      number += msg[i] - '0';
-     // Serial.write(msg[i]); // для отладки - пишем полученные данные в порт
-     //number = int(number);
-    } 
-    return number;
-  } 
 
 void recieve() {
   msgLen = VW_MAX_MESSAGE_LEN;
   if (vw_get_message(msg, &msgLen)) { // Если есть данные.. 
     digitalWrite(led_pin, HIGH); // Зажигаем светодиод в начале приема пакета 
-    if (msg[0] != 'v' && msg[1] != 'u' && msg[2] != 'l' && msg[3] != 'v')
+    if (msg[0] != 186 && msg[1] != 188 && msg[2] != 152 && msg[3] != 154)
       {
         return; 
       } 
      delay(1); // для отладки, чтобы видеть, когда мигает светодиод
      digitalWrite(led_pin, LOW); // Гасим светодиод в конце 
-     T = processing_data(4, 5);
-     H = processing_data(6, 7);
-     P = processing_data(8, 10);
-      
+     T = msg[4]; 
+     H = msg[5]; 
+     P = 700 + msg[6]; //Принимаем двузначное число. "7" прибавляем тут
+     display_intensity = map(msg[7], 0, 255, 0, 7);
      // Serial.print("T ");
-     // Serial.println(T);
+      Serial.println(msg[0]);
+      Serial.println(msg[1]);
+      Serial.println(msg[2]);
+      Serial.println(msg[3]);
+      Serial.println(msg[4]);
+      Serial.println(msg[5]);
+      Serial.println(msg[6]);
+      Serial.println(msg[7]);
      // Serial.print("B ");
      // Serial.println(H);
      // Serial.print("D ");
@@ -165,7 +154,7 @@ void power_control() {
     digitalWrite(PowerPin5, LOW);
     digitalWrite(ModePin3, HIGH);
     EEPROM.update(IONSTATE_adress, IONSTATE);
-    EEPROM.update(HEATSTATE_adress, HEATSTATE);
+    EEPROM.update(HEATERSTATE_adress, HEATERSTATE);
     EEPROM.update(HUMPOW_adress, HUMPOW);
     EEPROM.update(HUMVAL_adress, HUMVAL);
   }
@@ -207,12 +196,14 @@ void ion_control(){
 }
 
 void heat_control(){
-  if (PWRSTATE == 0 || PWRSTATE == 2 || PWRSTATE == 3 || HEATSTATE == 0){
+  if (PWRSTATE == 0 || PWRSTATE == 2 || PWRSTATE == 3 || HEATERSTATE == 0){
     digitalWrite(HeaterPin0, LOW);
   }
-  else if (HEATSTATE == 1){
+  else if (HEATERSTATE == 1){
     digitalWrite(HeaterPin0, HIGH);
   }
+  else if (PWRSTATE == 1 && HEATERSTATE == 2 && H < 40) digitalWrite(HeaterPin0, HIGH);
+  else if (PWRSTATE == 1 && HEATERSTATE == 2 && H > 40) digitalWrite(HeaterPin0, LOW);
 }
 
 void hum_control(){
@@ -233,14 +224,14 @@ void keyboard() {
         if (button == 16) inString = '2';
         if (button == 2) inString = '3';
         if (button == 8) inString = '4';
-        if (button == 10) inString = '5';
+        if (button == 32) inString = '5';
         if (button == 1) inString = '6';
         control(inString);
   } 
   delay(100);  
 }
 
-void irr() {
+void infraRed_control() {
   if (irrecv.decode(&results)) // Если данные пришли 
   {
     //Serial.println(results.value); // Отправляем полученную данную в консоль
@@ -252,17 +243,6 @@ void irr() {
     if (results.value == 338831067) control('5');
     if (results.value == 2737486129) control('6');
     irrecv.resume(); // Принимаем следующую команду
-  }
-}
-
-void web() {
-  char inString;
-  if (mySerial.available() > 0) {
-    inString = mySerial.read();
-    Serial.println(inString);
-      
-    control(inString);
-    delay(5);
   }
 }
   
@@ -279,13 +259,18 @@ void control(int val){
       if (IONSTATE < 1) IONSTATE++;
       else IONSTATE = 0;
       uartsend();
+      Display = 2;
+      previousMillis = currentMillis;
       Serial.println("CASE2");
       break;
-    case '3': // HEATSTATE
-      if (HEATSTATE < 1) HEATSTATE++;
-      else HEATSTATE = 0;
+    case '3': // HEATERSTATE
+      if (HEATERSTATE < 2) HEATERSTATE++;
+      else HEATERSTATE = 0;
       uartsend();
+      Display = 1;
+      previousMillis = currentMillis;
       Serial.println("CASE3");
+      Serial.println(HEATERSTATE);
       break;
     case '4': // HUMPOW
       if (HUMPOW < 3) HUMPOW++;
@@ -294,9 +279,11 @@ void control(int val){
       Serial.println("CASE4");
       break;
     case '5': // HUMVAL
-      if (HUMVAL < 81) HUMVAL += 5;
-      else HUMVAL = 45;
+      if (HUMVAL < 60) HUMVAL += 5;
+      else HUMVAL = 40;
       uartsend();
+      Display = 0;
+      previousMillis = currentMillis; 
       Serial.println("CASE5");
       break;
     case '6':
@@ -310,6 +297,31 @@ void control(int val){
 }
 
 void led_display() {
+  currentMillis = millis();
+  if(currentMillis - previousMillis < interval && Display == 0) {
+    disp.setIntensity(display_intensity);
+    disp.clearBuffer();
+    disp.setStatus(SETUP_LED, 1);
+    disp.putNumberAt(HUMVAL, 8, false, 10);
+    disp.writeBuffer(); 
+    return;
+  }
+  if(currentMillis - previousMillis < interval && Display == 1) {
+    disp.setIntensity(display_intensity);
+    disp.clearBuffer();
+    disp.setStatus(HEATER_LED, 1);
+    disp.putDigitAt((HEATERSTATE > 1 ? 10 : HEATERSTATE), 8);
+    disp.writeBuffer(); 
+    return;
+  }
+  if(currentMillis - previousMillis < interval && Display == 2) {
+    disp.setIntensity(display_intensity);
+    disp.clearBuffer();
+    disp.setStatus(ION_LED, 1);
+    disp.putDigitAt(IONSTATE, 8);
+    disp.writeBuffer(); 
+    return;
+  }
   disp.setIntensity(display_intensity);
   disp.clearBuffer();
   disp.putNumberAt(P, 4, false, 10);
@@ -318,9 +330,9 @@ void led_display() {
   disp.putNumberAt(T, 12, false, 10);
   disp.setStatus(GRADUS_LED, 1);
   disp.setStatus(POWER_LED, (PWR > 0 ? 0 : 1));
-  disp.setStatus(HEATER_LED, (HEATSTATE > 0 ? 1 : 0));
+  disp.setStatus(HEATER_LED, (digitalRead(HeaterPin0) == HIGH ? 1 : 0));
   disp.setStatus(NO_WATER_LED, (WATERSENSOR == LOW ? 1 : 0));
-  disp.setStatus(ION_LED, (IONSTATE > 0 ? 1 : 0));
+  disp.setStatus(ION_LED, (digitalRead(IonPin6) == HIGH ? 1 : 0));
   disp.setStatus(LEVEL_LED1, (HUMPOW == 1 || HUMPOW == 2 || HUMPOW == 3 ? 1 : 0));
   disp.setStatus(LEVEL_LED2, (HUMPOW == 1 || HUMPOW == 2 || HUMPOW == 3 ? 1 : 0));
   disp.setStatus(LEVEL_LED3, (HUMPOW == 1 || HUMPOW == 2 || HUMPOW == 3 ? 1 : 0));
@@ -339,8 +351,8 @@ void led_display() {
 
 void loop() {
 keyboard();
-web();
-irr();  
+web_control();
+infraRed_control();  
 power_control();
 mode_control();
 ion_control();
@@ -348,12 +360,20 @@ heat_control();
 hum_control();
 recieve();  
 led_display();
-
-  
-
 }
+  
+void web_control() {
+  char inString;
+  if (mySerial.available() > 0) {
+    inString = mySerial.read();
+    Serial.println(inString);
+    control(inString);
+    delay(5);
+  }
+}
+
 void uartsend(){
- String htmlsend; //= {T, "|", H, "|", "P", "|", PWRSTATE, "|", IONSTATE, "|", HEATSTATE, "|", HUMPOW, "|", HUMVAL};
+ String htmlsend; //= {T, "|", H, "|", "P", "|", PWRSTATE, "|", IONSTATE, "|", HEATERSTATE, "|", HUMPOW, "|", HUMVAL};
     htmlsend += T;
     htmlsend += "|";
     htmlsend += H;
@@ -364,7 +384,7 @@ void uartsend(){
     htmlsend += "|";
     htmlsend += IONSTATE;
     htmlsend += "|";
-    htmlsend += HEATSTATE;
+    htmlsend += HEATERSTATE;
     htmlsend += "|";
     htmlsend += HUMPOW;
     htmlsend += "|";
@@ -372,5 +392,5 @@ void uartsend(){
     htmlsend += "|";
     
     mySerial.print(htmlsend);
-   // Serial.println(htmlsend);
+    //Serial.println(htmlsend);
   }
--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -12,7 +12,7 @@ BMP085 dps = BMP085();
 
 float H; // значение влажности
 float T; // значение температуры
-long P; // давление
+int P; // давление
 const int led_pin = 13; // Пин светодиода 
 const int transmit_pin = 12; // Пин подключения передатчика
 const String RADIO_PASSWORD = "vulk";
@@ -24,8 +24,8 @@ void setup()
 {
   Serial.begin(9600); 
 // Датчик DHT 
-  dht.begin();
   Wire.begin(); 
+  dht.begin();
   dps.init();  
 // Передатчик 433МГц
   vw_set_tx_pin(transmit_pin); 
@@ -43,27 +43,27 @@ void tempsensor() {
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   // check if returns are valid, if they are NaN (not a number) then something went wrong!
-  H = dht.readHumidity();
+  H = dht.readHumidity() + 3;
+  delay(3000);
   T = dht.readTemperature();
   if (isnan(T) || isnan(H)) {
-    Serial.println("Failed to read from DHT");
-delay(1000);
+   return; // Serial.println("Failed to read from DHT");
   }
 }
 
 void barsensor() { 
  long Press;
-  dps.getPressure(&Press); 
-  P = Press / 133.3;
-//  long Temperature;
- //bmp.getTemperature(&Temperature); // Температура с датчика BMP180 не требуется
+ dps.getPressure(&Press); 
+ P = Press / 133.3;
+ //long Temperature_BMP180; // Температура с датчика BMP180 не требуется
+ //bmp.getTemperature(&Temperature_BMP180); // Температура с датчика BMP180 не требуется
 }
 
 void photosensor() {
   int val = analogRead(PIN_PHOTO_SENSOR);
   Brightness = map(val, 1023, 0, 0, 255); 
-  if (Brightness < 90) Brightness = 0;
-  else if (Brightness > 90) Brightness = 255;  
+ // if (Brightness < 90) Brightness = 0;
+ // else if (Brightness > 90) Brightness = 255;  
 } 
 
 // Вывод на диспоей
@@ -73,7 +73,6 @@ void display(float disp, byte x, byte y){
 //OzOled.setInverseDisplay(); 
   OzOled.setPageMode();            //Set addressing mode to Page Mode
   OzOled.setBrightness(Brightness);
-  Serial.println(Brightness);
   char tmp[10];
   dtostrf(disp, 0, 2, tmp);
   OzOled.printBigNumber(tmp, x, y, 4);
@@ -81,26 +80,18 @@ void display(float disp, byte x, byte y){
 
 // Передача данных по каналу 433МГц
 void transmit() { 
-  digitalWrite(led_pin, HIGH); // Зажигаем светодиод в начале передачи
-  int Ttransmit = round(T);
-  int Htransmit = round(H);
-//int Ptransmit = round(P);
-
-String strMsg = "vulk";
-  if (Ttransmit > -10 && Ttransmit < 10) strMsg += "0";
-  strMsg += Ttransmit;
-  if (Htransmit > -10 && Htransmit < 10) strMsg += "0";
-  strMsg += Htransmit;
-  strMsg += P;
-  char msg[20];
-  strMsg.toCharArray(msg, 20);
-  
+  byte msg[8];
+  msg[0] = 186; // Первая цифра пароля
+  msg[1] = 188; // Вторая цифра пароля
+  msg[2] = 152; // Третья цифра пароля
+  msg[3] = 154; // Четвертая цифра пароля
+  msg[4] = round(T); // Температура округленная
+  msg[5] = round(H); // Влажность округленная
+  msg[6] = P % 100; // Давление (без цифры 7, ее прибавим на приемнике)
+  msg[7] = Brightness;
   vw_send((uint8_t *)msg, strlen(msg)); // Отправка сообщения 
   vw_wait_tx(); // Ожидаем окончания отправки сообщения 
-  digitalWrite(led_pin, LOW); // Гасим светодиод в конце передачи 
-   
 }
-
 
 void loop()
 {
@@ -112,19 +103,19 @@ void loop()
   OzOled.printString("TEMP:");
   display(T, 2, 2);
   OzOled.printString("C",9,5);
-  delay(2000);
+  delay(3000);
   transmit();
   OzOled.clearDisplay();
   OzOled.printString("HUM: ");
   display(H, 2, 2);
   OzOled.printString("%",9,5);
-  delay(2000);
+  delay(3000);
   transmit();
   OzOled.clearDisplay();
   OzOled.printString("PRESS: ");
   display(P, 3, 2);
   OzOled.printString("MM",13,5);
-  delay(2000);
+  //delay(1000);
   
  // выводим для отладки:
  //   Serial.print("T: "); 
